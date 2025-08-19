1edb320bd4f09d7b26b5def682e7987301deebc2
FoilMiser <samsfoilcreations@gmail.com>
2025-08-19 15:07:03 -0700

    UX: hotkeys for pause and speed ([, ], Space)

---
+++ Assets/Scripts/LeniaHotkeys.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+[DefaultExecutionOrder(50)]
+public class LeniaHotkeys : MonoBehaviour
+{
+    public LeniaSimulator sim;
+    void Start(){ if(!sim) sim = FindObjectOfType<LeniaSimulator>(); }
+    void Update(){
+        if(!sim) return;
+        if (Input.GetKeyDown(KeyCode.Space)) sim.TogglePause();
+        if (Input.GetKeyDown(KeyCode.LeftBracket))  sim.SetStepsPerFrame(sim.stepsPerFrame-1);
+        if (Input.GetKeyDown(KeyCode.RightBracket)) sim.SetStepsPerFrame(sim.stepsPerFrame+1);
+    }
+}
