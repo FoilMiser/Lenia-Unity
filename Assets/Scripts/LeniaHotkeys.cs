60081e1872ba31a2c126bd03f28174a3ab69bbb8
FoilMiser <samsfoilcreations@gmail.com>
2025-08-20 02:32:28 -0700

    Hotkeys: route 1/2/3/4 through LeniaSeeder so Play and keys match

--- Assets/Scripts/LeniaHotkeys.cs
+++ Assets/Scripts/LeniaHotkeys.cs
@@ -1,51 +1,47 @@
 ﻿using UnityEngine;
-#if ENABLE_INPUT_SYSTEM && !ENABLE_LEGACY_INPUT_MANAGER
+#if ENABLE_INPUT_SYSTEM
 using UnityEngine.InputSystem;
 #endif
 
-[DefaultExecutionOrder(50)]
 public class LeniaHotkeys : MonoBehaviour
 {
     public LeniaSimulator sim;
+    public LeniaSeeder seeder;
 
-    void Start(){
-        if(!sim){
-            #if UNITY_2023_1_OR_NEWER
-            sim = Object.FindFirstObjectByType<LeniaSimulator>();
-            #else
-            sim = Object.FindObjectOfType<LeniaSimulator>();
-            #endif
+    void Awake(){
+        if (!sim)    sim    = FindFirstObjectByType<LeniaSimulator>();
+        if (!seeder) seeder = FindFirstObjectByType<LeniaSeeder>();
+    }
+
+    void ReseedWith(SeedMode mode){
+        if (seeder){
+            seeder.mode = mode;
+            seeder.SeedOnce();
+        } else if (sim){
+            // Fallbacks if seeder is missing; edit if your signatures differ.
+            switch(mode){
+                case SeedMode.Noise:   sim.SeedNoise(); break;
+                case SeedMode.Clusters: sim.SeedClusters(); break;
+                case SeedMode.Movers:  sim.SeedMovers(); break;
+                case SeedMode.Orbium:  sim.SeedOrbium(); break;
+            }
         }
     }
 
     void Update(){
-        if(!sim) return;
-
-        #if ENABLE_INPUT_SYSTEM && !ENABLE_LEGACY_INPUT_MANAGER
-        var kb = Keyboard.current; if (kb==null) return;
-        if (kb.spaceKey.wasPressedThisFrame) sim.TogglePause();
-        if (kb.leftBracketKey.wasPressedThisFrame)  sim.SetStepsPerFrame(sim.stepsPerFrame - 1);
-        if (kb.rightBracketKey.wasPressedThisFrame) sim.SetStepsPerFrame(sim.stepsPerFrame + 1);
-        if (kb.rKey.wasPressedThisFrame) {
-            #if UNITY_2023_1_OR_NEWER
-            var seeder = Object.FindFirstObjectByType<LeniaSeeder>();
-            #else
-            var seeder = Object.FindObjectOfType<LeniaSeeder>();
-            #endif
-            if (seeder) seeder.SeedNoise();
-        }
-        #else
-        if (Input.GetKeyDown(KeyCode.Space)) sim.TogglePause();
-        if (Input.GetKeyDown(KeyCode.LeftBracket))  sim.SetStepsPerFrame(sim.stepsPerFrame - 1);
-        if (Input.GetKeyDown(KeyCode.RightBracket)) sim.SetStepsPerFrame(sim.stepsPerFrame + 1);
-        if (Input.GetKeyDown(KeyCode.R)) {
-            #if UNITY_2023_1_OR_NEWER
-            var seeder = Object.FindFirstObjectByType<LeniaSeeder>();
-            #else
-            var seeder = Object.FindObjectOfType<LeniaSeeder>();
-            #endif
-            if (seeder) seeder.SeedNoise();
-        }
-        #endif
+#if ENABLE_INPUT_SYSTEM
+        var k = Keyboard.current; if (k == null) return;
+        if (k.rKey.wasPressedThisFrame)      ReseedWith(SeedMode.Noise);
+        if (k.digit1Key.wasPressedThisFrame) ReseedWith(SeedMode.Noise);
+        if (k.digit2Key.wasPressedThisFrame) ReseedWith(SeedMode.Clusters);
+        if (k.digit3Key.wasPressedThisFrame) ReseedWith(SeedMode.Movers);
+        if (k.digit4Key.wasPressedThisFrame) ReseedWith(SeedMode.Orbium);
+#else
+        if (Input.GetKeyDown(KeyCode.R))        ReseedWith(SeedMode.Noise);
+        if (Input.GetKeyDown(KeyCode.Alpha1))   ReseedWith(SeedMode.Noise);
+        if (Input.GetKeyDown(KeyCode.Alpha2))   ReseedWith(SeedMode.Clusters);
+        if (Input.GetKeyDown(KeyCode.Alpha3))   ReseedWith(SeedMode.Movers);
+        if (Input.GetKeyDown(KeyCode.Alpha4))   ReseedWith(SeedMode.Orbium);
+#endif
     }
 }
